28718187c2ce5669f93b3bde33d60ee0a1190341
Aswin s <aswinsudhakaran98@gmail.com>
2023-03-23 12:05:09 +0530

    update

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,12 +1,14 @@
 
 
 const express=require('express');
-const { loginController, registerController } = require('../controller/userCtrl');
+const { loginController, registerController, authController } = require('../controller/userCtrl');
+const authMiddleware = require('../middlewares/authMiddleware');
 
 // router object
 const router=express.Router()
 
 router.post('/login',loginController)
 router.post('/register',registerController)
+router.post('/getUserData',authMiddleware,authController)
 
 module.exports=router
